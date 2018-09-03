81eb9d9566a9e76b50bfa1e08084afd84d34534a
jmangz <echou1@binghamton.edu>
2018-09-02 20:22:12 -0400

    test

---
+++ javascript/algorithms/add-binary.js
@@ -0,0 +1,31 @@
+var addBinary = function(a, b) {
+    var [carry, i, j, res] = [0, a.length - 1, b.length - 1, ""];
+    while(i >= 0 || j >= 0 || carry > 0) {
+        const bita = i >= 0 ? Number(a[i]) : 0;
+        const bitb = j >= 0 ? Number(b[j]) : 0;
+        const sum = bita + bitb + carry;
+        res = `${sum%2}${res}`;
+        carry = Math.floor(sum / 2);
+        i--; j--;
+    }
+    return res;
+};
+
+function addBinary(a, b) {
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let result = '';
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const bita = i >= 0 ? Number(a[i]) : 0;
+    const bitb = j >= 0 ? Number(b[j]) : 0;
+    const sum = bita + bitb + carry;
+    result = `${sum % 2}${result}`;
+    carry = Math.floor(sum / 2);
+    i -= 1;
+    j -= 1;
+  }
+
+  return result;
+}
