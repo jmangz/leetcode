9e4a2ad05f93703013e5c6cf5845281b510df033
jmangz <echou1@binghamton.edu>
2018-09-24 18:55:31 -0400

    solved longest common prefix

--- javascript/algorithms/longest-common-prefix.js
+++ javascript/algorithms/longest-common-prefix.js
@@ -16,7 +16,21 @@
 // All given inputs are in lowercase letters a-z.
 
 function longestCommonPrefix(strs) {
+  if (strs.length === 0) return '';
+  let current = strs[0];
 
+  strs.slice(1).forEach((el) => {
+    while (el.indexOf(current) !== 0) {
+      current = current.slice(0, current.length - 1);
+      if (current === '') return '';
+    }
+  });
+
+  return current;
 }
 
-// 
+// Take the first element in the array and check if that string
+// is the same as the second string. If not, then we slice off
+// characters until this is true. Once it is true we move on to
+// the next element. If none it is never true, then we return an
+// empty string.
