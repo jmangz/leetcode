2c1ca765666d57398b1ba18c0596d46cd917cb2b
jmangz <echou1@binghamton.edu>
2018-10-12 12:46:07 -0400

    solved minimum size subarray sum

---
+++ javascript/algorithms/209-minimum-size-subarray-sum.js
@@ -0,0 +1,31 @@
+// Given an array of n positive integers and a positive integer s,
+// find the minimal length of a contiguous subarray of which the sum ≥ s.
+// If there isn't one, return 0 instead.
+//
+// Example:
+//
+// Input: s = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+// Follow up:
+// If you have figured out the O(n) solution, try coding another
+// solution of which the time complexity is O(n log n).
+
+function minSubArrayLen(s, nums) {
+  const { length } = nums;
+  let result = Number.MAX_SAFE_INTEGER;
+  let left = 0;
+  let sum = 0;
+
+  for (let i = 0; i < length; i += 1) {
+    const num = nums[i];
+    sum += num;
+    while (sum >= s) {
+      result = Math.min(result, i + 1 - left);
+      sum -= nums[left];
+      left += 1;
+    }
+  }
+
+  return result !== Number.MAX_SAFE_INTEGER ? result : 0;
+}
